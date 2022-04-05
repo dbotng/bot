e2216b087db80928e94e9c75f7f6d547cdb1d3a3
Featyre <twooter.0g179@simplelogin.co>
2022-04-05 23:45:36 +0800

    Removed dist directory

--- dist/commands/info/typescript.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-async function default_1(interaction) {
-    await interaction.reply('Hello! This is D-bot 2.0 running on typescript');
-}
-exports.default = default_1;
