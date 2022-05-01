c262b109701ae14ff4a63fb63817622d30dc68e8
Featyre <twooter.0g179@simplelogin.co>
2022-05-02 01:29:07 +0800

    minor: removed return function type on info command and changed desc

--- src/commands/info.ts
+++ src/commands/info.ts
@@ -5,12 +5,12 @@ import * as typescript from './info/typescript'
 
 export const data = new SlashCommandBuilder()
     .setName('info')
-    .setDescription('Information')
+    .setDescription('Information commands')
     .addSubcommand((command) =>
         command.setName('typescript').setDescription('Welcome to typescript!')
     )
 
-export async function execute(interaction: CommandInteraction): Promise<void> {
+export async function execute(interaction: CommandInteraction) {
     switch (interaction.options.getSubcommand()) {
         case 'typescript': {
             await typescript.execute(interaction)
