0aa5f62e02533a0df67122a8c72d8103e56ee3fe
Featyre <twooter.0g179@simplelogin.co>
2022-05-02 01:35:01 +0800

    minor: removed unneeded eslint commits and imports

--- src/events/discordjs/interactionCreate.ts
+++ src/events/discordjs/interactionCreate.ts
@@ -1,7 +1,5 @@
 import { Interaction } from 'discord.js'
 import * as cooldown from '../../common/cooldown'
-import humanizeDuration from 'humanize-duration'
-import embedBuilder from '../../builders/embedBuilder'
 import errorEmbedBuilder from '../../builders/errorEmbedBuilder'
 
 export const name = 'interactionCreate'
@@ -16,10 +14,8 @@ export async function execute(interaction: Interaction) {
     if (!command) return
 
     try {
-        /* eslint-disable @typescript-eslint/no-non-null-assertion */
         cooldown.check(interaction)
         if (interaction.replied) return
-        /* eslint-enable */
         await command.execute(interaction)
         cooldown.create(interaction)
     } catch (error) {
