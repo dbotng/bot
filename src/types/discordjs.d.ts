7bbe63c723e88412d82a89d51a16d1ba0cc10181
Featyre <twooter.0g179@simplelogin.co>
2022-05-02 00:41:29 +0800

    minor: declaring modules for discord.js and global variables

--- src/types/discordjs.d.ts
+++ src/types/discordjs.d.ts
@@ -2,6 +2,7 @@ import { Collection } from 'discord.js'
 
 declare module 'discord.js' {
     export interface Client {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         commands: Collection<unknown, any>
     }
 }
