1c6642853c4d2350582c17d079f5a3d9cf941d06
Featyre <featyre.cyyss@aleeas.com>
2022-05-22 15:28:45 +0800

    feat(code): add segfault handler

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import * as discordjs from './clients/discordjs'
 import * as distube from './clients/distube'
 import 'dotenv/config'
+// eslint-disable-next-line node/no-unpublished-import
+import SegfaultHandler from 'segfault-handler'
 
 global.cooldown = new Map()
 global.musicQueues = new Map()
@@ -9,6 +11,8 @@ discordjs.init()
 discordjs.client.login(process.env.token)
 
 distube.init()
+
+SegfaultHandler.registerHandler('crash.log')
 
 process.on('unhandledRejection', (error) => {
     console.error('[discord.js]', error)
